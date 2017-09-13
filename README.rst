2256a2e54a046544d36d6afab96fd16a8e204804
Brandon Johnson <brandon.johnson.official@gmail.com>
2017-09-13 11:55:10 -0400

    ~

--- README.rst
+++
@@ -1,3 +0,0 @@
-COINWRAP
-========
-A simple wrapper designed around the coinmarketcap API and based on other guy.
--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup
 
 
-import pypandoc
-
 setup(
     setup_cfg=True,
     setup_requires=['coinwrap'],
     license=open('LICENSE').read(),
-    long_description=open('README.rst').read()
+    long_description=open('README.md').read()
 )
